c5ce86e33efd2bb2509e192e30e562cc83d4cb1d
Martin Matousek <martin.matousek@outlook.com>
2023-07-03 18:42:26 +0200

    Modify paths and return as json

--- tamedrift/urls.py
+++ tamedrift/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("wildrift_cn.urls")),
+    path("wildrift_cn/", include("wildrift_cn.urls")),
     path('admin/', admin.site.urls),
 ]
--- wildrift_cn/views.py
+++ wildrift_cn/views.py
@@ -3,7 +3,7 @@ from django.core import serializers
 from wildrift_cn.models import ChampionStatistic
 
 
-def index(request):
+def statistics(request):
     stats = ChampionStatistic.objects.all()
     sstats = serializers.serialize('json', stats)
-    return HttpResponse(sstats)
+    return HttpResponse(sstats, content_type="application/json")
