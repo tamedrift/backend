42a78e63b26c7112379a4cf01084c64fb4fdb900
Martin Matousek <martin.matousek@outlook.com>
2023-08-17 14:10:30 +0200

    Remove local database settings

--- tamedrift/settings.py
+++ tamedrift/settings.py
@@ -74,20 +74,7 @@ WSGI_APPLICATION = "tamedrift.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default="postgresql://postgres:postgres@localhost:5432/tamedrift",
-            conn_max_age=600,
-        )
-    }
+DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
